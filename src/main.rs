4e3e882cb2e2f454d3d0a04a339c16b72b279236
samuel orji <awesomeorji@gmail.com>
2022-09-11 15:22:25 +0100

    parsing done

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,27 @@ use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 use serde::Deserialize;
 use service::Service;
 use db::*;
+
+use serde_json::json;
+
+// fn main() {
+//     // The type of `john` is `serde_json::Value`
+//     let john = json!({
+//         "name": "John Doe",
+//         "age": 43,
+//         "phones": [
+//             "+44 1234567",
+//             "+44 2345678"
+//         ]
+//     });
+//     let res = &john["phoness"][0];
+//
+//     println!("first phone number: {}", john["phoness"][0]);
+//
+//     // Convert to a string of JSON and print it out
+//     println!("{}", john.to_string());
+//
+//  }
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let db = DB::new()?;
